44ec2e29c050e24df5dad3232e50afdd688b4d0c
alanuwu <alanbg772@gmail.com>
2025-02-28 21:57:45 -0600

    Cambios en el identificador de employees (ahora UUIDv4)

--- src/employees/employees.service.ts
+++ src/employees/employees.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateEmployeeDto } from './dto/create-employee.dto';
 import { UpdateEmployeeDto } from './dto/update-employee.dto';
+import { v4 as uuid } from "uuid";
 
 @Injectable()
 export class EmployeesService {
     private employees: CreateEmployeeDto[] = [{
-      id: 1,
+      id: uuid(),
       name: "Alberto",
       lastName: "Costas",
       phoneNumber: "4191202458"
     },
   {
-    id: 2,
+    id: uuid(),
     name: "Jose",
     lastName: "Perez",
     phoneNumber: "4142808910"
   }]
   create(createEmployeeDto: CreateEmployeeDto) {
-    createEmployeeDto.id = this.employees.length +1;
+    createEmployeeDto.id = uuid();
     this.employees.push(createEmployeeDto);
     return this.employees;
   }
@@ -26,27 +27,31 @@ export class EmployeesService {
     return this.employees;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     const employee = this.employees.filter((employee) => employee.id === id)[0];
+    if(!employee) throw new NotFoundException();
     return employee;
   }
 
-  update(id: number, updateEmployeeDto: UpdateEmployeeDto) {
+  update(id: string, updateEmployeeDto: UpdateEmployeeDto) {
     let employeeToUpdate = this.findOne(id);
     employeeToUpdate = {
       ...employeeToUpdate,
       ...updateEmployeeDto,
     }
+    if(employeeToUpdate) throw new NotFoundException();
     this.employees = this.employees.map((employee) => {
       if(employee.id  === id){
         employee == employeeToUpdate;
       }
       return employee;
     })
+  
     return employeeToUpdate;
   }
 
-  remove(id: number) {
+  remove(id: string) {
+    this.findOne(id);
     this.employees = this.employees.filter((employee) => employee.id != id );
     return this.employees;
   }
